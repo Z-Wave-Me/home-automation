bbb1719752fcec2925027eff88d4bc4028849c1a
Poltorak Serguei <ps@ps>
2013-09-24 03:30:12 +0400

    Minor

--- modules/BatteryPolling/index.js
+++ modules/BatteryPolling/index.js
@@ -26,34 +26,6 @@ inherits(BatteryPolling, AutomationModule);
 
 _module = BatteryPolling;
 
-/*
-ZWaveBatteryLowLevelWarningWidget = function (id, controller, zDeviceId, zInstanceId) {
-    ZWaveBatteryLowLevelWarningWidget.super_.call(this, id, controller, zDeviceId, zInstanceId);
-
-    this.deviceType = "probe";
-
-    var listBatteryLow = [];
-    for (var id in zway.devices) {
-        if (zway.devices[id].Battery) {
-            if (zway.devices[id].Battery.data.level.value <= this.config.warningLevel) {
-                listBatteryLow.push(id);
-            }
-        }
-    }
-    if (listBatteryLow.length) {
-        this.setMetricValue("probeTitle", "Battery is empty in devices:" + listBatteryLow.toString());
-    } else {
-        this.setMetricValue("probeTitle", "Batteries are all OK");
-    }
-}
-
-inherits(ZWaveBatteryEmptyWarningWidget, ZWaveDevice);
-
-ZWaveBatteryEmptyWarningWidget.prototype.dataPoints = function () {
-    return [this._dics().level];
-}
-*/
-
 // ----------------------------------------------------------------------------
 // --- Module instance initialized
 // ----------------------------------------------------------------------------
@@ -76,12 +48,6 @@ BatteryPolling.prototype.init = function (config) {
        zway.devices[id].Battery && zway.devices[id].Battery.Get();
       }
     });
-
-    /*
-    var widget = new ZWaveBatteryLowLevelWarningWidget("BatterLowLevelWidget", this.controller, 0, 0);
-    widget.bindToDatapoints();
-    this.controller.devices[widget.id] = widget;
-    */
 };
 
 // ----------------------------------------------------------------------------
