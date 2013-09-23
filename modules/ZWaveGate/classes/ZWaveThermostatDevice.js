eaa93816c4fb6cd6965a22a30074afb8e3c33280
Poltorak Serguei <ps@ps>
2013-09-23 16:32:23 +0400

    Thermostat vDev fix

--- modules/ZWaveGate/classes/ZWaveThermostatDevice.js
+++ modules/ZWaveGate/classes/ZWaveThermostatDevice.js
@@ -16,7 +16,7 @@ ZWaveThermostatDevice = function (id, controller, zDeviceId, zInstanceId) {
     this.deviceType = "climate";
     this.deviceSubType = "thermostat";
 
-    this.sensorAvailable = Object.keys(this._di().commandClasses).indexOf("49") >= 0;
+    this.sensorAvailable = Object.keys(this._di().commandClasses).indexOf("49") >= 0 && Object.keys(this._dic(49).data).indexOf("1") >= 0;
     this.modeAvailable = Object.keys(this._di().commandClasses).indexOf("64") >= 0;
     this.setPointAvailable = Object.keys(this._di().commandClasses).indexOf("67") >= 0;
 
