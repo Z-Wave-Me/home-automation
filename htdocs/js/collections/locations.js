a58ad399e42e605ca3b46c1e13c743a764f38092
Stanislav Morozov <r3b@seoarmy.ru>
2013-12-11 14:03:34 +0700

    Rewrite create locations method. add support post type request.

---
+++ htdocs/js/collections/locations.js
@@ -0,0 +1,46 @@
+define([
+    //libs
+    'backbone',
+    'models/device'
+], function (Backbone, DeviceM) {
+    'use strict';
+    var DevicesCollection =  Backbone.Collection.extend({
+        // model reference
+        model: DeviceM,
+        methodToURL: {
+            'read': '/devices/',
+            'create': '/devices/',
+            'update': '/devices/',
+            'delete': '/devices/'
+        },
+
+        url: function () {
+            var url = this.id !== undefined ? '/' + this.id : '';
+            return url;
+        },
+
+        sync: function (method, model, options) {
+            options = options || {};
+            options.url = model.methodToURL[method.toLowerCase()] + this.url();
+
+            if (this.updateTime !== undefined) {
+                options.data = {since: this.updateTime};
+            }
+
+            Backbone.sync(method, model, options);
+        },
+
+        parse: function (response, xhr) {
+            this.updateTime = response.data.updateTime;
+            return response.data.devices;
+        },
+
+        initialize: function () {
+           log('init devices');
+        }
+
+    });
+
+    return DevicesCollection;
+
+});
