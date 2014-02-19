0ff4b8631762ae6de86379c9e81fd713cf3a8e45
Stanislav Morozov <r3b@seoarmy.ru>
2014-02-19 21:56:13 +0700

    Add new features

--- htdocs/js/views/widgets/view.js
+++ htdocs/js/views/widgets/view.js
@@ -12,7 +12,7 @@ define([
     var WidgetsView = Backbone.View.extend({
         el: '#devices-container',
         initialize: function () {
-            _.bindAll(this, 'render', 'renderWidget', 'renderWidgets');
+            _.bindAll(this, 'render', 'renderWidget', 'renderWidgets', 'setHideEvents');
             var that = this;
             that.Devices = window.App.Devices;
             that.Locations = window.App.Locations;
@@ -76,13 +76,206 @@ define([
                 }
             });
         },
-        renderWidget: function (device, template) {
+        renderWidget: function (model) {
+            var that = this;
+            if (model.get('deviceType') === "probe" || model.get('deviceType') === "battery") {
+                that.renderProbe(model);
+            } else if (model.get('deviceType') === "fan") {
+                that.renderFan(model);
+            } else if (model.get('deviceType') === "switchMultilevel") {
+                that.renderMultilevel(model);
+            } else if (model.get('deviceType') === "thermostat") {
+                that.renderThermostat(model);
+            } else if (model.get('deviceType') === "doorlock") {
+                that.renderDoorlock(model);
+            } else if (model.get('deviceType') === "switchBinary") {
+                that.renderSwitch(model);
+            } else {
+                //log(model);
+            }
+        },
+        renderProbe: function (model) {
             var that = this,
-                json = device.toJSON(),
-                $template;
+                $ProbeTmp = $(_.template(templateProbe, model.toJSON()));
 
-            $template = $(_.template(template, json));
+            that.setHideEvents($ProbeTmp, model);
 
+            if (that.activeMode) {
+                $ProbeTmp.removeClass('clear');
+            } else {
+                $ProbeTmp.addClass('clear');
+            }
+
+
+            if (!that.isExistWidget(model.get('id'))) {
+                that.$el.append($ProbeTmp);
+            } else {
+                that.$el.find('div[data-widget-id="' + model.get('id') + '"]').replaceWith($ProbeTmp);
+            }
+        },
+        renderFan: function (model) {
+            var that = this,
+                $FanTmp = $(_.template(templateFan, model.toJSON()));
+
+            that.setHideEvents($FanTmp, model);
+
+            if (that.activeMode) {
+                $FanTmp.removeClass('clear');
+            } else {
+                $FanTmp.addClass('clear');
+            }
+
+
+            $FanTmp.find(".select-field select").on('change', function () {
+
+                var params = $(this).val() !== -1 ? {mode: $(this).val()} : {},
+                    command = $(this).val() !== -1 ? 'setMode' : 'off';
+
+                Apis.devices.command(model.get('id'), command, params, function (json) {
+                    //log(json);
+                });
+            });
+
+            if (!that.isExistWidget(model.get('id'))) {
+                that.$el.append($FanTmp);
+            } else {
+                that.$el.find('div[data-widget-id="' + model.get('id') + '"]').replaceWith($FanTmp);
+            }
+        },
+        renderDoorlock: function (model) {
+            var that = this,
+                $DoorLockTmp = $(_.template(templateDoorlock, model.toJSON()));
+
+            that.setHideEvents($DoorLockTmp, model);
+
+            if (that.activeMode) {
+                $DoorLockTmp.removeClass('clear');
+            } else {
+                $DoorLockTmp.addClass('clear');
+            }
+
+            $DoorLockTmp.find('.action').on('click', function (e) {
+                e.preventDefault();
+                var $button = $(this),
+                    command = !$button.hasClass('active') ? 'open' : 'closed';
+                Apis.devices.command(model.get('id'), command, {}, function (json) {
+                    //log(json);
+                    $button
+                        .toggleClass('active')
+                        .attr('title', command.capitalize())
+                        .children()
+                        .toggleClass('active')
+                        .find('.text')
+                        .text(command.toUpperCase());
+                });
+            });
+            if (!that.isExistWidget(model.get('id'))) {
+                that.$el.append($DoorLockTmp);
+            } else {
+                that.$el.find('div[data-widget-id="' + model.get('id') + '"]').replaceWith($DoorLockTmp);
+            }
+        },
+        renderMultilevel: function (model) {
+            var that = this,
+                $ComplementaryTmp = $(_.template(templateComplementary, model.toJSON())),
+                $range = $ComplementaryTmp.find('.input-range'),
+                $progress =  $ComplementaryTmp.find('.progress-bar'),
+                $text =  $ComplementaryTmp.find('.text');
+
+            that.setHideEvents($ComplementaryTmp, model);
+
+            if (that.activeMode) {
+                $ComplementaryTmp.removeClass('clear');
+            } else {
+                $ComplementaryTmp.addClass('clear');
+            }
+
+            $progress.on('mouseover', function () {
+                $progress.toggleClass('hidden');
+                $text.toggleClass('hidden');
+                $range.toggleClass('hidden');
+                $range.doVisibleRange();
+            });
+
+            $range.on('change', function () {
+                $range.doVisibleRange();
+            }).on('mouseout', function () {
+                    $text.toggleClass('hidden');
+                    $progress.val($range.val()).toggleClass('hidden');
+                    $range.toggleClass('hidden');
+                    Apis.devices.command(model.get('id'), 'exact', {level: $range.val()}, function (json) {
+                        //log(json);
+                    });
+                });
+
+            if (!that.isExistWidget(model.get('id'))) {
+                that.$el.append($ComplementaryTmp);
+            } else {
+                that.$el.find('div[data-widget-id="' + model.get('id') + '"]').replaceWith($ComplementaryTmp);
+            }
+        },
+        renderThermostat: function (model) {
+            var that = this,
+                $ThermostatTmp = $(_.template(templateThermostat, model.toJSON()));
+
+            that.setHideEvents($ThermostatTmp, model);
+
+            if (that.activeMode) {
+                $ThermostatTmp.removeClass('clear');
+            } else {
+                $ThermostatTmp.addClass('clear');
+            }
+
+            $ThermostatTmp.find(".select-field select").on('change', function () {
+
+                var params = $(this).val() !== -1 ? {mode: $(this).val()} : {},
+                    command = $(this).val() !== -1 ? 'setMode' : 'off';
+
+                Apis.devices.command(model.get('id'), command, params, function (json) {
+                    //log(json);
+                });
+            });
+            if (!that.isExistWidget(model.get('id'))) {
+                that.$el.append($ThermostatTmp);
+            } else {
+                that.$el.find('div[data-widget-id="' + model.get('id') + '"]').replaceWith($ThermostatTmp);
+            }
+        },
+        renderSwitch: function (model) {
+            var that = this,
+                $SwitchTmp = $(_.template(templateSwitch, model.toJSON()));
+
+            that.setHideEvents($SwitchTmp, model);
+
+            if (that.activeMode) {
+                $SwitchTmp.removeClass('clear');
+            } else {
+                $SwitchTmp.addClass('clear');
+            }
+
+            $SwitchTmp.find('.action').on('click', function (e) {
+                e.preventDefault();
+
+                var $button = $(this),
+                    command = !$button.hasClass('active') ? 'on' : 'off';
+
+                Apis.devices.command(model.get('id'), command, {}, function () {
+                    $button
+                        .toggleClass('active')
+                        .attr('title', command.capitalize())
+                        .children()
+                        .toggleClass('active')
+                        .find('.text').text(command.toUpperCase());
+                });
+            });
+            if (!that.isExistWidget(model.get('id'))) {
+                that.$el.append($SwitchTmp);
+            } else {
+                that.$el.find('div[data-widget-id="' + model.get('id') + '"]').replaceWith($SwitchTmp);
+            }
+        },
+        setHideEvents: function ($template, device) {
+            var that = this;
             that.listenTo(device, 'show', function () {
                 $template.removeClass('show').addClass('show').show('fast');
             });
@@ -90,12 +283,6 @@ define([
             that.listenTo(device, 'hide', function () {
                 $template.removeClass('show').hide('fast');
             });
-
-            if (!that.isExistWidget(device.get('id'))) {
-                that.$el.append($template);
-            } else {
-                that.$el.find('div[data-widget-id="' + device.get('id') + '"]').replaceWith($template);
-            }
         },
         isExistWidget: function (id) {
             return $('div[data-widget-id="' + id + '"]').exists();
