8f6800ea9e437112d429a8569187d29c8f9c040d
Stanislav Morozov <r3b@seoarmy.ru>
2014-02-23 23:17:40 +0700

    Remove moduleId

--- classes/AutomationController.js
+++ classes/AutomationController.js
@@ -153,12 +153,14 @@ AutomationController.prototype.loadModules = function (callback) {
             return "modules/" + moduleClassName;
         };
 
+        moduleMeta.id = moduleClassName;
+
         // Grab _module and clear it out
         self.modules[moduleClassName] = {
             meta: moduleMeta,
-            className: moduleClassName,
             classRef: _module
         };
+
         _module = undefined;
     });
 
@@ -180,20 +182,21 @@ AutomationController.prototype.loadModules = function (callback) {
 
 AutomationController.prototype.instantiateModule = function (instanceModel) {
 
+    console.log(JSON.stringify(instanceModel));
+
     var self = this,
         module = _.find(self.modules, function (module) { return instanceModel.moduleId === module.meta.id; }),
-        moduleClass = module.className,
-        instance = new window[moduleClass](instanceModel.id, self);
+        instance = new window[module.meta.id](instanceModel.id, self);
 
-    console.log("Instantiating module", instanceModel.id, "from class", moduleClass);
+    console.log("Instantiating module", instanceModel.id, "from class", module.meta.id);
 
     if (module.meta.singleton) {
-        if (in_array(self._loadedSingletons, moduleClass)) {
+        if (in_array(self._loadedSingletons, module.meta.id)) {
             console.log("WARNING: Module", instanceModel.id, "is a singleton and already has been instantiated. Skipping.");
             return;
         }
 
-        self._loadedSingletons.push(moduleClass);
+        self._loadedSingletons.push(module.meta.id);
     }
 
     instance.init(instance.params);
@@ -207,7 +210,7 @@ AutomationController.prototype.instantiateModules = function () {
 
     console.log("--- Automatic modules instantiation ---");
     self._autoLoadModules.forEach(function (moduleClassName) {
-        module = _.find(self.modules, function (module) { return module.className === moduleClassName; });
+        module = _.find(self.modules, function (module) { return module.id === moduleClassName; });
         if (!!module && !_.any(self.instances, function (model) { return model.moduleId === module.meta.id; })) {
             self.createInstance(module.meta.id, module.meta.defaults);
         }
