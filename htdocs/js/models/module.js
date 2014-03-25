f51e51a290682d797503e5a91b6456748b520f05
Stanislav Morozov <r3b@seoarmy.ru>
2014-03-25 18:53:43 +0700

    Fix alpaca less

--- htdocs/js/models/module.js
+++ htdocs/js/models/module.js
@@ -153,15 +153,15 @@ define([
             // options
             if (options) {
                 Object.keys(defaultObject).forEach(function (key) {
-                    if (!options.hasOwnProperty(key)) {
-                        options[key] = defaultObject[key];
-                    }
+                    options[key] = defaultObject[key];
                 });
+
 
                 if (options.hasOwnProperty('fields')) {
                     Object.keys(options.fields).forEach(function (key) {
                         if (!options.fields[key].hasOwnProperty('helpers')) {
                             options.fields[key].helper = "";
+                            options.fields[key].toolbarSticky = true;
                         }
                     });
                 }
