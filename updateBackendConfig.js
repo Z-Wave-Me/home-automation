cd1fe155c4e9edcd9344be501c8de10a34572a42
Stanislav Morozov <r3bfish@gmail.com>
2014-11-10 11:28:47 +0300

    - move actualize func to updateBackendConfig.js

--- updateBackendConfig.js
+++ updateBackendConfig.js
@@ -61,6 +61,138 @@
     for (var indx in config.instances) {
       fixObject(config.instances[indx].params);
     }
+
+    // profiles
+    if (config.hasOwnProperty('profiles')) {
+      if (config.profiles.length > 0) {
+        config.profiles.forEach(function (profile) {
+          if (profile.hasOwnProperty('groups')) {
+            delete profile.groups;
+          }
+
+          if (profile.hasOwnProperty('active')) {
+            delete profile.active;
+          }
+
+          if (_.isArray(profile.positions)) {
+            profile.positions = _.filter(profile.positions, function (position) {
+              return _.isString(position);
+            });
+          } else {
+            profile.positions = [];
+          }
+        });
+      } else {
+        config.profiles = [];
+      }
+    }
+
+    // instances
+    if (config.hasOwnProperty('instances')) {
+      if (config.instances.length > 0) {
+        config.instances.forEach(function (instance) {
+          // move title and description params
+          instance.title = instance.params.title;
+          instance.description = instance.params.description;
+          delete instance.params.title;
+          delete instance.params.description;
+
+          // move status
+          if (instance.params.hasOwnProperty('status')) {
+            instance.active = instance.params.status === 'enable';
+            delete instance.params.status;
+          } else if (!instance.hasOwnProperty('active')) {
+            instance.active = true;
+          }
+
+          // delete userView
+          if (instance.hasOwnProperty('userView')) {
+            delete instance.userView;
+          }
+        });
+      }
+    }
+
+    config.modules_categories = [
+      {
+        "id": "automation",
+        "name": "Automation",
+        "description": "Create home automation rules",
+        "icon": ""
+      },
+      {
+        "id": "security",
+        "name": "Security",
+        "description": "Enhance security",
+        "icon": ""
+      },
+      {
+        "id": "peripherals",
+        "name": "Peripherals",
+        "description": "Z-Wave and other peripherals",
+        "icon": ""
+      },
+      {
+        "id": "surveillance",
+        "name": "Video surevillance",
+        "description": "Support for cameras",
+        "icon": ""
+      },
+      {
+        "id": "logging",
+        "name": "Data logging",
+        "description": "Logging to third party services",
+        "icon": ""
+      },
+      {
+        "id": "scripting",
+        "name": "Scripting",
+        "description": "Create custom scripts",
+        "icon": ""
+      },
+      {
+        "id": "devices",
+        "name": "Devices",
+        "description": "Create devices",
+        "icon": ""
+      },
+      {
+        "id": "scheduling",
+        "name": "Schedulers",
+        "description": "Time related functions",
+        "icon": ""
+      },
+      {
+        "id": "climate",
+        "name": "Climate",
+        "description": "Climate control",
+        "icon": ""
+      },
+      {
+        "id": "environment",
+        "name": "Environment",
+        "description": "Environment related date",
+        "icon": ""
+      },
+      {
+        "id": "scenes",
+        "name": "Scenes",
+        "description": "Light scenes",
+        "icon": ""
+      },
+      {
+        "id": "notifications",
+        "name": "Notifications",
+        "description": "SMS, E-mail and push notifications",
+        "icon": ""
+      },
+      {
+        "id": "tagging",
+        "name": "Tagging",
+        "description": "Tagging widgets",
+        "icon": ""
+      }
+    ];
     
     saveObject("config.json", config);
   }
