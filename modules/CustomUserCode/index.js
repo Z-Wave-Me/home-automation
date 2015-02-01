c098d7a4547752102037e2a329c6be3fc3542d48
jensmoes <jens.troest@gmail.com>
2015-01-31 17:24:25 -0800

    Add delay timer for executing script. Useful if you want to be sure that all devices are created or you just want something to happen a certain time after startup

--- modules/CustomUserCode/index.js
+++ modules/CustomUserCode/index.js
@@ -31,16 +31,41 @@ CustomUserCode.prototype.init = function (config) {
     // Call superclass' init (this will process config argument and so on)
     CustomUserCode.super_.prototype.init.call(this, config);
 
-    // TODO: executeJS errors are impossible to catch!
-    //try {
-        executeJS(this.config.customCode);
-    //} catch (e) {
-    //    controller.addNotification("warning", "Failed to load custom user code: " + this.get("metrics:title"), "module");
-    //}
+    var self = this;
+
+    this.timer = null;
+
+    this.timeoutHandler = this.handler;
+
+    if(self.timer) {
+	clearTimeout(self.timer);
+    }
+    self.timer = setTimeout(function() {
+	self.timeoutHandler();
+    }, self.config.delay*1000);
+    
+};
+
+CustomUserCode.prototype.stop = function () {
+
+    if(this.timer) {
+	clearTimeout(this.timer);
+    }
+
+    this.timeoutHandler = function () {};
+
+    CustomUserCode.super_.prototype.stop.call(this);
 };
 
 // ----------------------------------------------------------------------------
 // --- Module methods
 // ----------------------------------------------------------------------------
 
-// This module has no additional methods
+CustomUserCode.prototype.handler = function() {
+    // TODO: executeJS errors are impossible to catch!
+    //try {
+        executeJS(this.config.customCode);
+    //} catch (e) {
+    //    controller.addNotification("warning", "Failed to load custom user code: " + this.get("metrics:title"), "module");
+    //}    
+};
