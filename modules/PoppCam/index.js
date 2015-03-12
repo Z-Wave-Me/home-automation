39a1eedb0d44c1a575cae39436dcb42bdcb8da42
Niels Roche <nir@zwave.eu>
2015-03-12 16:27:24 +0100

    fixed bug in PopCam module - seems that setting up authentication is not working

--- modules/PoppCam/index.js
+++ modules/PoppCam/index.js
@@ -56,7 +56,9 @@ _.extend(PoppCam.prototype, {
             });
         };
         
-		this.setup 	= config.url+ "/login.xml?user=admin&password=admin&usr=admin&pwd=admin";  
+        /* setting up the autentications seems not to work anymore
+        
+		this.setup 	= config.url+ "/login.xml?user=" + config.user + "&password=" + config.password + "&usr=" + config.user + "&pwd=" + config.password;  
 		if (this.setup) {
             http.request({
                 url: this.setup,
@@ -67,8 +69,9 @@ _.extend(PoppCam.prototype, {
                 }
             });
         }
+        */
 		    
-        this.url	= config.url + "/videostream.cgi?stream=1";        
+        this.url	= config.url + "/videostream.cgi?user=" + config.user + "&password=" + config.password;        
         ws.proxify(this.proxy_url, this.url, config.user, config.password);
 
         this.vDev = this.controller.devices.create({
