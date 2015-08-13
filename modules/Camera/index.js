293aa12246b7134428b2bd7f751b47983e44732f
aivs <aivs@yandex.ru>
2015-08-13 08:31:26 +0300

    Now you can control dimmers and blinds from web camera widget

--- modules/Camera/index.js
+++ modules/Camera/index.js
@@ -39,7 +39,7 @@ _.extend(Camera.prototype, {
                 
                 if (vDev) {
                     var type = vDev.get("deviceType");
-                    if (type === "switchBinary") {
+                    if (type === "switchBinary" || "switchMultilevel") {
                         vDev.performCommand(command == "open" ? "on" : "off");
                     } else if (type === "doorlock") {
                             vDev.performCommand(command);
