368dbaf1e3d1d50ee7e712d8678191a2cd2244e4
Niels Roche <nroche.z-wave@outlook.de>
2016-10-24 15:38:12 +0200

    ImportRemoteHA 2.0.1:  * add functionality to tag all remote widgets  * enhance url input to add ip adress only (with backward compatibility)

--- modules/ImportRemoteHA/index.js
+++ modules/ImportRemoteHA/index.js
@@ -1,9 +1,9 @@
 /*** ImportRemoteHA Z-Way HA module *******************************************
 
-Version: 2.0.0
+Version: 2.0.1
 (c) Z-Wave.Me, 2014
 -----------------------------------------------------------------------------
-Author: Poltorak Serguei <ps@z-wave.me>
+Author: Poltorak Serguei <ps@z-wave.me>, Niels Roche <nir@zwave.eu>
 Description:
     Imports devices from remote HA engine
 ******************************************************************************/
@@ -29,8 +29,12 @@ ImportRemoteHA.prototype.init = function (config) {
     ImportRemoteHA.super_.prototype.init.call(this, config);
 
     var self = this;
+
+    var config_url = this.config.url.indexOf('http://') > -1? this.config.url : 'http://' + this.config.url + ':8083';
+
+    console.log('config_url',config_url);
     
-    this.urlPrefix = this.config.url + "/ZAutomation/api/v1/devices";
+    this.urlPrefix = config_url + "/ZAutomation/api/v1/devices";
     this.dT = Math.max(this.config.dT, 500); // 500 ms minimal delay between requests
     this.timestamp = 0;
     this.lastRequest = 0;
@@ -127,7 +131,7 @@ ImportRemoteHA.prototype.parseResponse = function (response) {
                     return;
                 }
 
-                self.controller.devices.create({
+                var dev = {
                     deviceId: localId,
                     defaults: {
                         deviceType: item.deviceType,
@@ -138,7 +142,13 @@ ImportRemoteHA.prototype.parseResponse = function (response) {
                     },
                     overlay: {},
                     moduleId: this.id
-                });
+                }
+
+                if (self.config.addTag) {
+                    dev.overlay.tags = self.config.tagName && self.config.tagName !== ''? [self.config.tagName] : ["RemoteHA_" + self.id];
+                }
+
+                self.controller.devices.create(dev);
 
                 self.config.renderDevices.push({deviceId: localId, deviceType: item.deviceType});
                 self.saveConfig();
