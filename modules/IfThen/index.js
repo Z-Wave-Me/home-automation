cfddbb4f6d52643edffaad0190cf071e0adb8176
Yurkin Vitaliy <aivs@z-wave.me>
2017-07-08 01:23:07 +0300

    IfThen. Don't send On command, if device is already turned On, similarly for Off

--- modules/IfThen/index.js
+++ modules/IfThen/index.js
@@ -33,31 +33,31 @@ IfThen.prototype.init = function (config) {
     this.handlerLevel = function (sDev) {
         var that = self,
             operator = ifElement.operator,
-            ifLevel = (ifElement.status === 'level' && ifElement.level) || (!ifElement.status && ifElement.level) ? ifElement.level : ifElement.status,
+            ifLevel = (ifElement.status === "level" && ifElement.level) || (!ifElement.status && ifElement.level) ? ifElement.level : ifElement.status,
             check = false,
             value = sDev.get("metrics:level");
 
         if (operator && ifLevel) {
             switch (operator) {
-                case '>':
+                case ">":
                     check = value > ifLevel;
                     break;
-                case '=':
+                case "=":
                     check = value === ifLevel;
                     break;
-                case '<':
+                case "<":
                     check = value < ifLevel;
                     break;
             }
         }
 
-        if(check || value === ifLevel || sDev.get('deviceType') === 'toggleButton'){
+        if(check || value === ifLevel || sDev.get("deviceType") === "toggleButton"){
             self.config.targets.forEach(function(el) {
                 var type = el.filterThen,
                     id = el[type].target,
-                    lvl = el[type].status === 'level' && el[type].level? el[type].level : (el[type].status === 'color' && el[type].color? el[type].color: el[type].status),
+                    lvl = el[type].status === "level" && el[type].level? el[type].level : (el[type].status === "color" && el[type].color? el[type].color: el[type].status),
                     vDev = that.controller.devices.get(id),
-                    // compare old and new level to avoid unneccessary updates
+                    // compare old and new level
                     compareValues = function(valOld,valNew){
                         var vO = _.isNaN(parseFloat(valOld))? valOld : parseFloat(valOld),
                             vN = _.isNaN(parseFloat(valNew))? valNew : parseFloat(valNew);
@@ -65,21 +65,22 @@ IfThen.prototype.init = function (config) {
                         return vO !== vN;
                     },
                     set = compareValues(vDev.get("metrics:level"), lvl);
-                
-                if (vDev && set) {
-                    if (vDev.get("deviceType") === type && (type === "switchMultilevel" || type === "thermostat" || type === "switchRGBW")) {
-                        if (lvl === 'on' || lvl === 'off'){
+
+                if (vDev) {
+                    if (!el[type].sendAction || (el[type].sendAction && set)) {
+                        if (vDev.get("deviceType") === type && (type === "switchMultilevel" || type === "thermostat" || type === "switchRGBW")) {
+                            if (lvl === "on" || lvl === "off"){
+                                vDev.performCommand(lvl);
+                            } else if (typeof lvl === "object") {
+                                vDev.performCommand("exact", lvl);
+                            } else {
+                                vDev.performCommand("exact", { level: lvl });
+                            }
+                        } else if (vDev.get("deviceType") === "toggleButton" && type === "scene") {
+                            vDev.performCommand("on");
+                        } else if (vDev.get("deviceType") === type) {
                             vDev.performCommand(lvl);
-                        } else if (typeof lvl === 'object') {
-                            vDev.performCommand("exact", lvl);
-                        } else {
-                            vDev.performCommand("exact", { level: lvl });
                         }
-                    } else if (vDev.get("deviceType") === "toggleButton" && type === "scene") {
-                        vDev.performCommand("on");
-                    } else if (vDev.get("deviceType") === type) {
-                        
-                        vDev.performCommand(lvl);
                     }
                 }
             });
@@ -88,7 +89,7 @@ IfThen.prototype.init = function (config) {
 
     // Setup metric update event listener
     if(ifElement && ifElement.device){
-        self.controller.devices.on(ifElement.device, 'change:metrics:level', self.handlerLevel);
+        self.controller.devices.on(ifElement.device, "change:metrics:level", self.handlerLevel);
     }
 };
 
@@ -96,7 +97,7 @@ IfThen.prototype.stop = function () {
     var self = this;
     
     // remove event listener
-    self.controller.devices.off(self.config.sourceDevice[self.config.sourceDevice.filterIf].device,'change:metrics:level', self.handlerLevel);
+    self.controller.devices.off(self.config.sourceDevice[self.config.sourceDevice.filterIf].device,"change:metrics:level", self.handlerLevel);
 
     IfThen.super_.prototype.stop.call(this);
 };
