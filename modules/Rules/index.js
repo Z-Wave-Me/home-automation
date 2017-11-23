8a8bdf92cbde0aad03ec42bc7373b30d58e7a895
Niels Roche <nroche.z-wave@outlook.de>
2017-11-23 10:47:37 +0100

    add Modules Rules, Schedules, Scenes for new SHUI control feature

---
+++ modules/Rules/index.js
@@ -0,0 +1,123 @@
+/*** IfThen Z-Way HA module *******************************************
+
+Version: 2.5.1
+(c) Z-Wave.Me, 2017
+-----------------------------------------------------------------------------
+Author: Niels Roche <nir@zwave.eu>
+Author: Hans-Christian Göckeritz <hcg@zwave.eu>
+Author: Karsten Reichel <kar@zwave.eu>
+Description:
+	Bind actions on one device to other devices or scenes
+******************************************************************************/
+
+// ----------------------------------------------------------------------------
+// --- Class definition, inheritance and setup
+// ----------------------------------------------------------------------------
+
+function IfThen (id, controller) {
+	// Call superconstructor first (AutomationModule)
+	IfThen.super_.call(this, id, controller);
+}
+
+inherits(IfThen, AutomationModule);
+
+_module = IfThen;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+IfThen.prototype.init = function (config) {
+	IfThen.super_.prototype.init.call(this, config);
+
+	var self = this,
+		ifElement = self.config.sourceDevice[self.config.sourceDevice.filterIf];
+	this.handlerLevel = function (sDev) {
+		var that = self,
+			operator = ifElement.operator,
+			ifLevel = (ifElement.status === 'level' && ifElement.level) || (!ifElement.status && ifElement.level) ? ifElement.level : ifElement.status,
+			check = false,
+			value = sDev.get("metrics:level");
+
+		if (operator && ifLevel) {
+			switch (operator) {
+				case '>':
+					check = value > ifLevel;
+					break;
+				case '=':
+					check = value === ifLevel;
+					break;
+				case '<':
+					check = value < ifLevel;
+					break;
+			}
+		}
+
+		if(check || value === ifLevel || sDev.get('deviceType') === 'toggleButton'){
+
+			self.config.targets.forEach(function(el) {
+				var type = el.filterThen;
+
+				if(type === "notification") {
+					if(typeof el.notification.target !== 'undefined' || typeof el.notification.mail_to_input !== 'undefined') {
+						var mail;
+						if(el.notification.target.search('@') > 0 || (mail = typeof el.notification.mail_to_input !== 'undefined')) {
+							self.addNotification('mail.notification', typeof el.notification.message === 'undefined' ? 'Source: ' + JSON.stringify(self.config.sourceDevice) + ' Target: ' + JSON.stringify(self.config.targets) : el.notification.message, mail ? el.notification.mail_to_input : el.notification.target);
+						} else {
+							self.addNotification('push.notification', typeof el.notification.message === 'undefined' ? 'Source: ' + JSON.stringify(self.config.sourceDevice) + ' Target: ' + JSON.stringify(self.config.targets) : el.notification.message, el.notification.target);
+						}
+					}
+				} else {
+					var id = el[type].target,
+						lvl = el[type].status === 'level' && el[type].level? el[type].level : (el[type].status === 'color' && el[type].color? el[type].color: el[type].status),
+						vDev = that.controller.devices.get(id),
+						// compare old and new level to avoid unneccessary updates
+						compareValues = function(valOld,valNew){
+							var vO = _.isNaN(parseFloat(valOld))? valOld : parseFloat(valOld),
+								vN = _.isNaN(parseFloat(valNew))? valNew : parseFloat(valNew);
+
+							return vO !== vN;
+						},
+						set = compareValues(vDev.get("metrics:level"), lvl);
+
+
+					//if (vDev && set) {
+					if (vDev && (!el[type].sendAction || (el[type].sendAction && set))) {
+
+						if (vDev.get("deviceType") === type && (type === "switchMultilevel" || type === "thermostat" || type === "switchRGBW")) {
+							if (lvl === 'on' || lvl === 'off'){
+								vDev.performCommand(lvl);
+							} else if (typeof lvl === 'object') {
+								vDev.performCommand("exact", lvl);
+							} else {
+								vDev.performCommand("exact", { level: lvl });
+							}
+						} else if (vDev.get("deviceType") === "toggleButton" && type === "scene") {
+							vDev.performCommand("on");
+						} else if (vDev.get("deviceType") === type) {
+							vDev.performCommand(lvl);
+						}
+					}
+				}
+			});
+		}
+	};
+
+	// Setup metric update event listener
+	if(ifElement && ifElement.device){
+		self.controller.devices.on(ifElement.device, 'change:metrics:level', self.handlerLevel);
+	}
+};
+
+IfThen.prototype.stop = function () {
+	var self = this;
+	
+	// remove event listener
+	self.controller.devices.off(self.config.sourceDevice[self.config.sourceDevice.filterIf].device,'change:metrics:level', self.handlerLevel);
+
+	IfThen.super_.prototype.stop.call(this);
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
---
+++ modules/Scenes/index.js
@@ -0,0 +1,104 @@
+/*** LightScene Z-Way HA module *******************************************
+
+Version: 1.1.1
+(c) Z-Wave.Me, 2017
+-----------------------------------------------------------------------------
+Author: Poltorak Serguei <ps@z-wave.me>
+Changed: Michael Hensche <mh@zwave.eu>
+Description:
+	Implements light scene based on virtual devices of type dimmer, switch or anothe scene
+******************************************************************************/
+
+// ----------------------------------------------------------------------------
+// --- Class definition, inheritance and setup
+// ----------------------------------------------------------------------------
+
+function LightScene (id, controller) {
+	// Call superconstructor first (AutomationModule)
+	LightScene.super_.call(this, id, controller);
+}
+
+inherits(LightScene, AutomationModule);
+
+_module = LightScene;
+
+// ----------------------------------------------------------------------------
+// --- Module instance initialized
+// ----------------------------------------------------------------------------
+
+LightScene.prototype.init = function (config) {
+	LightScene.super_.prototype.init.call(this, config);
+
+	var self = this;
+
+	this.vDev = this.controller.devices.create({
+		deviceId: "LightScene_" + this.id,
+		defaults: {
+			deviceType: "toggleButton",
+			metrics: {
+				level: "on", // it is always on, but usefull to allow bind
+				icon: "scene",
+				title: self.getInstanceTitle()
+			}
+		},
+		overlay: {},
+		handler: function (command) {
+			if (command !== 'on') return;
+
+			self.config.switches.forEach(function(devState) {
+				var vDev = self.controller.devices.get(devState.device);
+				if (vDev) {
+					if (!devState.sendAction || (devState.sendAction && vDev.get("metrics:level") != devState.status)) {
+						vDev.performCommand(devState.status);
+					}
+				}
+			});
+			self.config.thermostats.forEach(function(devState) {
+				var vDev = self.controller.devices.get(devState.device);
+				if (vDev) {
+					if (!devState.sendAction || (devState.sendAction && vDev.get("metrics:level") != devState.status)) {
+						vDev.performCommand("exact", { level: devState.status });
+					}
+				}
+			});
+			self.config.dimmers.forEach(function(devState) {
+				var vDev = self.controller.devices.get(devState.device);
+				if (vDev) {
+					if (!devState.sendAction || (devState.sendAction && vDev.get("metrics:level") != devState.status)) {
+						vDev.performCommand("exact", { level: devState.status });
+					}
+				}
+			});
+			self.config.locks.forEach(function(devState) {
+				var vDev = self.controller.devices.get(devState.device);
+				if (vDev) {
+					if (!devState.sendAction || (devState.sendAction && vDev.get("metrics:level") != devState.status)) {
+						vDev.performCommand(devState.status);
+					}
+				}
+			});
+			self.config.scenes.forEach(function(scene) {
+				var vDev = self.controller.devices.get(scene);
+				if (vDev) {
+					vDev.performCommand("on");
+				}
+			});
+
+			self.vDev.set("metrics:level", "on"); // update on ourself to allow catch this event
+		},
+		moduleId: this.id
+	});
+};
+
+LightScene.prototype.stop = function () {
+	if (this.vDev) {
+		this.controller.devices.remove(this.vDev.id);
+		this.vDev = null;
+	}
+
+	LightScene.super_.prototype.stop.call(this);
+};
+
+// ----------------------------------------------------------------------------
+// --- Module methods
+// ----------------------------------------------------------------------------
