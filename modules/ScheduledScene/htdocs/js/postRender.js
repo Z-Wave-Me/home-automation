eb3ec1b6c546a5238e1967f139ff2ff57d32bb82
hcg-zwave <30596548+hcg-zwave@users.noreply.github.com>
2017-12-11 15:55:47 +0100

    MOUDLES-32
    automatically Name on Weekdays and first time entry
    and bugfix showing mobile devices and email addresses in scene select

---
+++ modules/ScheduledScene/htdocs/js/postRender.js
@@ -0,0 +1,50 @@
+function modulePostRender(control) {
+    if( $('input[name=title]').val() === 'Schedule')
+	{
+	    week = getWeekArray();
+	    $('#title').val(week + ': ' + $('input[name=times_0]').val());
+    }
+
+    $(document).on("click", "input[type=checkbox]", function() {
+        if(window.location.href.indexOf("post") > -1) {
+            week = getWeekArray();
+            $('#title').val(week + ': ' + $('input[name=times_0]').val());
+	    }
+    });
+
+    $(document).on("blur", "input[name=times_0]", function() {
+        if(window.location.href.indexOf("post") > -1) {
+            week = getWeekArray();
+            $('#title').val(week + ': ' + $('input[name=times_0]').val());
+	    }
+    });
+}
+
+function getWeekArray() {
+    var weekdays = ['Su', 'Mo', 'Tue', 'Wed', 'Thu', 'Fr', 'Sa'];
+    var elem = ($('div[data-alpaca-field-name=weekdays]'))[0].children,
+        lday = -1,
+        a_week = [],
+        a_week_counter = 0;
+
+    for(var i = 0, len = elem.length; i<len; i++) {
+        var check = $(elem[i]).find("input"),
+            day = parseInt($(check).attr('data-checkbox-value'));
+
+        if($(check).is(':checked')) {
+            if(lday === -1) {
+                a_week[a_week_counter++] = weekdays[day];
+                lday = day;
+                continue;
+            }
+            if((lday + 1) === day || (lday - 6) === day) {
+                a_week[a_week_counter] = '-' + weekdays[day];
+            } else {
+                a_week_counter++;
+                a_week[a_week_counter++] = ',' + weekdays[day];
+            }
+            lday = day;
+        }
+    };
+    return a_week.join("");
+}
