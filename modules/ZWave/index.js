4d58a0b263ba7c4d04cc2f7c3d778eae7602ec64
Yurkin Vitaliy <aivs@z-wave.me>
2018-04-18 12:09:51 +0300

    Gas Alarm (V7) 0x12 support

--- modules/ZWave/index.js
+++ modules/ZWave/index.js
@@ -5508,6 +5508,10 @@ ZWave.prototype.parseAddCommandClass = function (nodeId, instanceId, commandClas
 									a_defaults.metrics.icon = 'alarm';
 									a_defaults.probeType = 'alarm_clock';
 									break;
+								case 0x12: // Gas Alarm (V7)
+									a_defaults.metrics.icon = 'alarm';
+									a_defaults.probeType = 'alarmSensor_co';
+									break;
 								default:
 									return; // skip this type
 							}
