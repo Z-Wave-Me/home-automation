4a2f559731041f800da9806abe5fae46a993e045
Poltorak Serguei <ps@z-wave.me>
2019-10-30 10:19:56 +0300

    Fixed reconfiguration bug - instance was created twice

--- classes/AutomationController.js
+++ classes/AutomationController.js
@@ -1027,7 +1027,7 @@ AutomationController.prototype.reconfigureInstance = function(id, instanceObject
 		if (!!register_instance) {
 			if (this.instances[index].active) { // here we read new config instead of existing
 				register_instance.init(config);
-				//this.registerInstance(register_instance);
+				this.registerInstance(register_instance);
 			} else {
 				register_instance.saveNewConfig(config);
 			}
