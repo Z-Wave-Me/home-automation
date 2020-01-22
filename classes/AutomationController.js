864ed0abab0272e77110cb81c9ec65440bdae7e0
Poltorak Serguei <ps@z-wave.me>
2020-01-23 02:19:36 +0300

    AutomationController.loadModuleMedia handles MIME types

--- classes/AutomationController.js
+++ classes/AutomationController.js
@@ -2780,10 +2780,60 @@ AutomationController.prototype.loadMainLang = function(pathPrefix) {
 };
 
 AutomationController.prototype.loadModuleMedia = function(moduleName, fileName) {
-	var img = ["png", "PNG", "jpg", "jpeg", "JPG", "JPEG", "gif", "GIF"],
-		text = ["css", "js", "txt", "rtf", "xml"],
-		html = ["htm", "html", "shtml"],
-		video = ["mpeg", "mpg", "mpe", "qt", "mov", "viv", "vivo", "avi", "movie", "mp4"],
+	var contentTypes = {
+			"png": "image/png",
+			"jpg": "image/jpeg",
+			"jpeg": "image/jpeg",
+			"gif": "image/gif",
+			
+			"css": "text/css",
+			"js": "text/javascript",
+			"txt": "text/txt",
+			"rtf": "text/rtf",
+			"xml": "text/xml",
+			
+			"htm": "text/htm",
+			"html": "text/html",
+			"shtml": "text/shtml",
+			
+			"mpeg": "video/mpeg",
+			"mpg": "video/mpg",
+			"mpe": "video/mpe",
+			"qt": "video/qt",
+			"mov": "video/mov",
+			"viv": "video/viv",
+			"vivo": "video/vivo",
+			"avi": "video/avi",
+			"movie": "video/movie",
+			"mp4": "video/mp4",
+			
+			"doc": "application/msword",
+			"dot": "application/msword",
+			"docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+			"dotx": "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+			"docm": "application/vnd.ms-word.document.macroEnabled.12",
+			"dotm": "application/vnd.ms-word.template.macroEnabled.12",
+			"xls": "application/vnd.ms-excel",
+			"xlt": "application/vnd.ms-excel",
+			"xla": "application/vnd.ms-excel",
+			"xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+			"xltx": "application/vnd.openxmlformats-officedocument.spreadsheetml.template",
+			"xlsm": "application/vnd.ms-excel.sheet.macroEnabled.12",
+			"xltm": "application/vnd.ms-excel.template.macroEnabled.12",
+			"xlam": "application/vnd.ms-excel.addin.macroEnabled.12",
+			"xlsb": "application/vnd.ms-excel.sheet.binary.macroEnabled.12",
+			"ppt": "application/vnd.ms-powerpoint",
+			"pot": "application/vnd.ms-powerpoint",
+			"pps": "application/vnd.ms-powerpoint",
+			"ppa": "application/vnd.ms-powerpoint",
+			"pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+			"potx": "application/vnd.openxmlformats-officedocument.presentationml.template",
+			"ppsx": "application/vnd.openxmlformats-officedocument.presentationml.slideshow",
+			"ppam": "application/vnd.ms-powerpoint.addin.macroEnabled.12",
+			"pptm": "application/vnd.ms-powerpoint.presentation.macroEnabled.12",
+			"potm": "application/vnd.ms-powerpoint.presentation.macroEnabled.12",
+			"ppsm": "application/vnd.ms-powerpoint.slideshow.macroEnabled.12",
+		},
 		fe,
 		resObject = {
 			data: null,
@@ -2791,35 +2841,18 @@ AutomationController.prototype.loadModuleMedia = function(moduleName, fileName) 
 		};
 
 	try {
-
-		fe = fileName.split(".").pop();
-
-		if (img.indexOf(fe) > -1) {
-			resObject.ct = "image/(png|jpeg|gif)";
-		}
-
-		if (text.indexOf(fe) > -1) {
-			resObject.ct = "text/(css|html|javascript|plain|rtf|xml)";
-		}
-
-		if (html.indexOf(fe) > -1) {
-			resObject.ct = "text/html";
-		}
-
-		if (video.indexOf(fe) > -1) {
-			resObject.ct = "video/(mpeg|quicktime|vnd.vivo|x-msvideo|x-sgi-movie|mp4)";
-		}
-
+		fe = fileName.split(".").pop().toLowerCase();
+		resObject.ct = contentTypes[fe] || "text/plain";
+		
 		try {
 			resObject.data = fs.load('userModules/' + moduleName + '/htdocs/' + fileName);
 		} catch (e) {
 			resObject.data = fs.load('modules/' + moduleName + '/htdocs/' + fileName);
 		}
-
 	} catch (e) {
 		resObject = null;
 	}
-
+	
 	return resObject;
 };
 
